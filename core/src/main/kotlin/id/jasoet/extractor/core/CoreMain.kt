9cb283a5084307843d6787d1976223e9f4071d89
Deny Prasetyo <jasoet87@gmail.com>
2016-04-18 09:35:53 +0700

    Add Apache Tika Deps

--- core/src/main/kotlin/id/jasoet/extractor/core/CoreMain.kt
+++ core/src/main/kotlin/id/jasoet/extractor/core/CoreMain.kt
@@ -1,7 +1,17 @@
 package id.jasoet.extractor.core
 
 import org.slf4j.LoggerFactory
-
+//import java.io.File
+//import java.io.FileInputStream
+//import java.io.IOException
+//
+//import org.apache.tika.exception.TikaException
+//import org.apache.tika.metadata.Metadata
+//import org.apache.tika.parser.ParseContext
+//import org.apache.tika.parser.pdf.PDFParser
+//import org.apache.tika.sax.BodyContentHandler
+//
+//import org.xml.sax.SAXException
 /**
  * Documentation Here
  *
