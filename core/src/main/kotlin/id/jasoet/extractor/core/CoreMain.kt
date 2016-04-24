c8b3b32644cbb3c5ecb2bfa6b3a28d354fb05c23
Deny Prasetyo <jasoet87@gmail.com>
2016-04-24 18:47:41 +0700

    Something

--- core/src/main/kotlin/id/jasoet/extractor/core/CoreMain.kt
+++ core/src/main/kotlin/id/jasoet/extractor/core/CoreMain.kt
@@ -1,17 +1,17 @@
 package id.jasoet.extractor.core
 
 import org.slf4j.LoggerFactory
-//import java.io.File
-//import java.io.FileInputStream
-//import java.io.IOException
-//
-//import org.apache.tika.exception.TikaException
-//import org.apache.tika.metadata.Metadata
-//import org.apache.tika.parser.ParseContext
-//import org.apache.tika.parser.pdf.PDFParser
-//import org.apache.tika.sax.BodyContentHandler
-//
-//import org.xml.sax.SAXException
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+
+import org.apache.tika.exception.TikaException
+import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.ParseContext
+import org.apache.tika.parser.pdf.PDFParser
+import org.apache.tika.sax.BodyContentHandler
+
+import org.xml.sax.SAXException
 /**
  * Documentation Here
  *
@@ -23,6 +23,21 @@ object CoreMain {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        log.info("Do Nothing Only To Satisfy Spring Boot Plugin")
+        val handler = BodyContentHandler();
+        val metadata = Metadata();
+        val inputStream = CoreMain.javaClass.getResourceAsStream("/Example.pdf");
+        val parseContext = ParseContext();
+
+        //parsing the document using PDF parser
+        val pdfParser = PDFParser();
+        pdfParser.parse(inputStream, handler, metadata,parseContext);
+
+        log.info("Content of the Pdf : ${handler.toString()}")
+        log.info("Metadata of the PDF:");
+        val metadataNames = metadata.names();
+
+        metadataNames.forEach {
+            log.info("$it : ${metadata.get(it)}")
+        }
     }
 }
