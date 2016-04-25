4d239622ce40d9177d3da76c280ba66d32add552
Deny Prasetyo <jasoet87@gmail.com>
2016-04-26 03:50:52 +0700

    Optimize Imports

--- core/src/main/kotlin/id/jasoet/extractor/core/CoreMain.kt
+++ core/src/main/kotlin/id/jasoet/extractor/core/CoreMain.kt
@@ -1,17 +1,12 @@
 package id.jasoet.extractor.core
 
-import org.slf4j.LoggerFactory
-import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-
-import org.apache.tika.exception.TikaException
 import org.apache.tika.metadata.Metadata
 import org.apache.tika.parser.ParseContext
 import org.apache.tika.parser.pdf.PDFParser
 import org.apache.tika.sax.BodyContentHandler
+import org.slf4j.LoggerFactory
 
-import org.xml.sax.SAXException
+
 /**
  * Documentation Here
  *
@@ -30,7 +25,7 @@ object CoreMain {
 
         //parsing the document using PDF parser
         val pdfParser = PDFParser();
-        pdfParser.parse(inputStream, handler, metadata,parseContext);
+        pdfParser.parse(inputStream, handler, metadata, parseContext);
 
         log.info("Content of the Pdf : ${handler.toString()}")
         log.info("Metadata of the PDF:");
