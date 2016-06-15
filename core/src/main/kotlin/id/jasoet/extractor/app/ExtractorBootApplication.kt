b38a0e02ad85e6a14a9da602cd1017b451186cb6
Deny Prasetyo <jasoet87@gmail.com>
2016-06-15 12:03:30 +0700

    Add fast claspath scanner, test it

--- core/src/main/kotlin/id/jasoet/extractor/app/ExtractorBootApplication.kt
+++ core/src/main/kotlin/id/jasoet/extractor/app/ExtractorBootApplication.kt
@@ -3,10 +3,12 @@ package id.jasoet.extractor.app
 import id.jasoet.extractor.app.loader.loadDocumentModel
 import id.jasoet.extractor.app.model.DocumentModel
 import id.jasoet.extractor.app.model.ProcessedDocument
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner
 import org.mongodb.morphia.Datastore
 import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
 
 /**
@@ -50,6 +52,12 @@ open class ExtractorBootApplication {
                 dataStore.save(processedDocument)
                 log.info("Saved Processed Document")
             }
+
+            val classWithAnnotation = FastClasspathScanner("id.jasoet.extractor.app.config")
+                .scan()
+                .getNamesOfClassesWithAnnotation(Configuration::class.java)
+
+            log.info("Class with Annotation ${classWithAnnotation.reduce { s, i -> "$s | $i" }}")
             log.info("Application Running in ${currentDirectory()}")
         }
     }
