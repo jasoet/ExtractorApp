3defe46c40e731541c1a5463b734ac1c40b205b7
Deny Prasetyo <jasoet87@gmail.com>
2016-06-25 04:13:53 +0700

    Change to use new Print Color

--- core/src/main/kotlin/id/jasoet/extractor/app/command/handler/AddHandler.kt
+++ core/src/main/kotlin/id/jasoet/extractor/app/command/handler/AddHandler.kt
@@ -3,6 +3,7 @@ package id.jasoet.extractor.app.command.handler
 import id.jasoet.extractor.app.command.AddCommand
 import id.jasoet.extractor.app.printc
 import id.jasoet.extractor.app.workingDirectory
+import org.fusesource.jansi.Ansi
 import org.springframework.stereotype.Component
 
 /**
@@ -15,11 +16,11 @@ import org.springframework.stereotype.Component
 class AddHandler {
 
     fun handle(command: AddCommand) {
-        printc {
-            fgBrightGreen().a("Working Dir: ${workingDirectory()}")
+        printc(Ansi.Color.GREEN) {
+            "Working Dir: ${workingDirectory()}"
         }
-        printc {
-            fgBrightRed().a("Process Add Command $command")
+        printc(Ansi.Color.RED) {
+            "Process Add Command $command"
         }
     }
 
